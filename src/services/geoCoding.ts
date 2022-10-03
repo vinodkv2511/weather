b8dd235dabac2bfb0f340ec9535edda7a54e23bf
vinodkv2511 <vinodkv2511@gmail.com>
2022-10-04 04:38:09 +0530

    Makes geo coding api https

--- src/services/geoCoding.ts
+++ src/services/geoCoding.ts
@@ -7,7 +7,7 @@ const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 export const geoCodingApi = createApi({
   reducerPath: 'geoApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://api.openweathermap.org/geo/1.0/'}),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.openweathermap.org/geo/1.0/'}),
   endpoints: (builder) => ({
     getLocation: builder.query<GeoCodingResponse, GeoCodingRequest>({ 
       query: ({q, limit}) => `direct?q=${q}&limit=${limit || ''}&appid=${API_KEY}`
